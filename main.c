cfe203b24c91d8c1b722a23affc5f0cea9575433
YanLu <yfl5541@psu.edu>
2020-09-18 12:26:38 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int digit_sum(int n) {
+  if  (n < 10){
+    return n;
+  }
+  else {
+    return (n%10+digit_sum(n/10));
+  }
+
+}
+
+int main(void) {
+  int a = atof(readline("Enter an int: "));
+  printf("sum of digits of %d is %d.\n",a,digit_sum(a));
+  return 0;
+}
